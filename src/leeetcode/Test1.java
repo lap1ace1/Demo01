25c302b5dee4d7188fa6c55f0b60ee8f7e560f60
lhl <2609485452@qq.com>
2023-05-05 17:47:49 +0800

    leetcode1

--- src/leeetcode/Test1.java
+++ src/leeetcode/Test1.java
@@ -13,7 +13,7 @@ public class Test1 {
     public static  int[] twoSum(int[] nums, int target) {
         for(int i=0;i< nums.length;i++){
             for (int j =0;j< nums.length;j++){
-                if (nums[i]+nums[j]==target){
+                if (i!=j&&nums[i]+nums[j]==target){
                     return new int[]{i,j};
                 }
             }
