84271c77a6ff7924bfd3f8fdd01036e9dcf4404b
lhl <2609485452@qq.com>
2023-05-12 14:06:09 +0800

    leetcode27

--- src/leeetcode/LeetCode27.java
+++ src/leeetcode/LeetCode27.java
@@ -1,2 +1,22 @@
-package leeetcode;public class LeetCode27 {
+package leeetcode;
+
+import java.util.Arrays;
+
+public class LeetCode27 {
+    public static void main(String[] args) {
+        int[] a ={1,2,3,4,5};
+        int val = 3;
+        System.out.println(quchu(a,val));
+
+    }
+    public static int quchu(int[] nums, int val){
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]!=val){
+                nums[index] = nums[i];
+                index++;
+            }
+
+        }return index;
+    }
 }
