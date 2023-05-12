ae67737988ccc3d37480a24147396908559f63c8
lhl <2609485452@qq.com>
2023-05-12 14:05:18 +0800

    leetcode27

---
+++ src/leeetcode/LeetCode27.java
@@ -0,0 +1,2 @@
+package leeetcode;public class LeetCode27 {
+}
--- src/leeetcode/leetcode27.java
+++ src/leeetcode/leetcode27.java
@@ -2,21 +2,21 @@ package leeetcode;
 
 import java.util.Arrays;
 
-public class leetcode27 {
+public class LeetCode27 {
     public static void main(String[] args) {
-        int[] a= {1,2,2,3};
-        int val =2;
-        System.out.println(Arrays.toString(sczd(a,val)));
+        int[] a ={1,2,3,4,5};
+        int val = 3;
+        System.out.println(quchu(a,val));
 
     }
-    public static int[] sczd(int[] nums,int val){
-        int suoyin = 0;
+    public static int quchu(int[] nums, int val){
+        int index = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i]!=val) {
-                nums[suoyin] = nums[i];
-                suoyin++;
+            if (nums[i]!=val){
+                nums[index] = nums[i];
+                index++;
             }
-        }
-        return nums;
+
+        }return index;
     }
 }
