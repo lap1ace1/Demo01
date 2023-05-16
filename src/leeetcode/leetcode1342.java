70c2bedba4179906fe202c0408131b83ca50f356
lhl <2609485452@qq.com>
2023-05-16 13:34:04 +0800

    leetcode69 1342

---
+++ src/leeetcode/leetcode1342.java
@@ -0,0 +1,21 @@
+package leeetcode;
+
+public class leetcode1342 {
+    public static void main(String[] args) {
+        int a = 12;
+        System.out.println(bianzero(a));
+
+    }
+    public static int bianzero(int num){
+        int res = 0;
+        while (num>0){
+            if (num%2==0){
+                 num=num/2;
+                 res++;
+            }if (num%2!=0){
+                num=num-1;
+                res++;}
+
+        }return res;
+    }
+}
---
+++ src/leeetcode/leetcode69.java
@@ -0,0 +1,22 @@
+package leeetcode;
+
+public class leetcode69 {
+    public static void main(String[] args) {
+        int x=8;
+        System.out.println(genhao(8));
+
+    }
+    public static int genhao(int x){
+        int l=0,r=x,ans=-1 ;
+        while (l<=r){
+            int mid = l + (r - l) / 2;
+            if ((long)mid*mid<=x){
+                ans = mid;
+                l=mid+1;
+            }else {
+                r = mid-1;
+            }
+        }
+        return ans;
+    }
+}
