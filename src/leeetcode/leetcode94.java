8610f4080b459232486d7c256e609ad84cd48842
lhl <2609485452@qq.com>
2023-05-21 10:53:24 +0800

    leetcode94

--- src/leeetcode/leetcode94.java
+++ src/leeetcode/leetcode94.java
@@ -5,43 +5,44 @@ import java.util.List;
 
 public class leetcode94 {
     public static void main(String[] args) {
-        TreeNode treeNode = new TreeNode(1,null,new TreeNode(2,new TreeNode(3)));
-        List<Integer> integers = inorderTraversal(treeNode);
-        System.out.println();
+
+        TreeNode treeNode = new TreeNode(3,new TreeNode(5), new TreeNode(2));
+        System.out.println(inorderTraversal(treeNode));
     }
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        TreeNode pre = null;
-        while (root!=null){
-            if (root.left!=null){
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        TreeNode pre=null;
+        while (root != null) {
+            if (root.left != null) {
                 pre = root.left;
-                while (pre.right!=null&&pre.right!=root){
+                while (pre.right != null && pre.right != root) {
                     pre = pre.right;
                 }
-                if (pre.right==null){
-                    pre.right=root;
-                    root=root.left;
-                }else {
+                if (pre.right == null) {
+                    pre.right = root;
+                    root = root.left;
+                } else {
                     res.add(root.val);
-                    root=root.right;
+                    root = root.right;
                 }
-            }else {
+            } else {
                 res.add(root.val);
-                root=root.right;
+                root = root.right;
             }
-        }return res;
+        }
+        return res;
     }
     public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) { this.val = val; }
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
 }
-
