c028dbb9e9f3cbc2b214062b83ea2dc1b555e7f8
lhl <2609485452@qq.com>
2023-05-27 18:02:43 +0800

    String

--- src/Test/Test05.java
+++ src/Test/Test05.java
@@ -5,7 +5,7 @@ import java.util.Locale;
 public class Test05 {
     public static void main(String[] args) {
         String str = "this is a test";
-        //1.将str中的单词单独获取出来
+         //1.将str中的单词单独获取出来
         //2.将str中的test替换为practice
         //3.在test前面插入一个easy
         //4.将每个单词的字母该为大写
