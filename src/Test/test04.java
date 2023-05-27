41e34562020336bdbf1ea2d5a4d03857bc00330a
lhl <2609485452@qq.com>
2023-05-27 18:28:47 +0800

    test

--- src/Test/test04.java
+++ src/Test/test04.java
@@ -4,7 +4,7 @@ public class test04 {
     public static void main(String[] args) {
         for (int i = 1; i <=9 ; i++) {
             for (int j = 1; j <i ; j++) {
-                System.out.print(i+"*"+j+"="+i*j+"\t");
+                System.out.print(i+"* "+j+"="+i*j+"\t");
             }System.out.println();
         }
     }
