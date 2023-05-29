48a983d6348ee8f025b279c834cbe17aa0cd56d2
lhl <2609485452@qq.com>
2023-05-29 18:01:41 +0800

    leetcode125

---
+++ src/leeetcode/leetcode125.java
@@ -0,0 +1,26 @@
+package leeetcode;
+
+public class leetcode125 {
+    public static void main(String[] args) {
+        String a = "sjdlayh";
+        System.out.println(hwc(a));
+
+    }
+    public static boolean hwc(String s){
+        StringBuffer a = new StringBuffer();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isLetterOrDigit(ch)){
+                a.append(Character.toLowerCase(ch));
+            }
+        }
+        int str = a.length()/2;
+        for (int i = 0; i < str; i++) {
+            char v1 = a.charAt(i);
+            char v2 = a.charAt(a.length()-1-i);
+            if (v1!=v2){
+                return false;
+            }
+        }return true;
+    }
+}
