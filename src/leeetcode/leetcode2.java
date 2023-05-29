3cede192a7e842fe29f32e40f3d0e0b48414f51b
lhl <2609485452@qq.com>
2023-05-29 17:55:44 +0800

    leetcode2

---
+++ src/leeetcode/leetcode2.java
@@ -0,0 +1,56 @@
+package leeetcode;
+
+public class leetcode2 {
+    public static void main(String[] args) {
+        ListNode a1 = new ListNode(1,new ListNode(2,new ListNode(3)));
+        ListNode a2 = new ListNode(1,new ListNode(2,new ListNode(3)));
+        ListNode res =sum2(a1,a2);
+        while (res!=null){
+            System.out.print(res.val+"  ");
+            res = res.next;
+        }
+    }
+
+
+
+    public static ListNode sum2(ListNode l1,ListNode l2){
+        ListNode res = new ListNode();
+        ListNode cur = res;
+        int carry = 0;
+        while (l1!=null||l2!=null){
+            int x=l1==null?0: l1.val;
+            int y=l2==null?0: l2.val;
+            int sum=x+y+carry;
+            carry=sum/10;
+            cur.next = new ListNode(sum%10);
+            cur=cur.next;
+            if (l1!=null){
+                l1=l1.next;
+            }
+            if (l2!=null){
+                l2=l2.next;
+            }
+        }
+        if (carry==1){
+            cur.next=new ListNode(1);
+        }if (res.next!=null){
+            res=res.next;
+        }return res;
+
+    }
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }}
