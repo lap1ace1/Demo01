9edfbfbd1defe73feeb6525215ce20763255e727
lhl <2609485452@qq.com>
2023-07-23 19:22:11 +0800

    复习

--- src/rewlet/let121.java
+++ src/rewlet/let121.java
@@ -2,6 +2,19 @@ package rewlet;
 
 public class let121 {
     public static void main(String[] args) {
-
+        int[] a  = {7,1,5,3,6,4};
+        System.out.println(timing(a));
+    }
+    public static int timing(int[] prices){
+        int max = 0;
+        for (int i = 0; i < prices.length-1; i++) {
+            for (int j = i+1; j < prices.length ; j++) {
+                int sb = prices[j]-prices[i];
+                if (sb>max){
+                    max=sb;
+                }
+            }
+        }
+        return max;
     }
 }
