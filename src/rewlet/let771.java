faebbe21d4b826da4138fd94f1d062ab63681cb7
lhl <2609485452@qq.com>
2023-07-24 19:12:51 +0800

    771

---
+++ src/rewlet/let771.java
@@ -0,0 +1,22 @@
+package rewlet;
+
+public class let771 {
+    public static void main(String[] args) {
+        String j = "aAa";
+        String s = "AaAbbbb";
+        System.out.println(numJewelsInStones(j,s));
+    }
+    public static int numJewelsInStones(String jewels, String stones) {
+        int st = 0;
+        for (int i = 0; i < stones.length(); i++) {
+            char v1 = stones.charAt(i);
+            for (int j = 0; j < jewels.length(); j++) {
+                char v2 = jewels.charAt(j);
+                if (v2==v1){
+                    st++;
+                    break;
+                }
+            }
+        }return st;
+    }
+}
