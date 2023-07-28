b9f1594f74c003e2ecf95408e58ce3915b15d018
lhl <2609485452@qq.com>
2023-07-28 16:34:11 +0800

    1768

---
+++ src/rewlet/let1768.java
@@ -0,0 +1,26 @@
+package rewlet;
+
+public class let1768 {
+    public static void main(String[] args) {
+        String w1 = "abcd";
+        String w2 = "defa";
+        System.out.println(strjt(w1,w2));
+    }
+    public static String strjt(String word1,String word2){
+        int m =word1.length();//3
+        int n =word2.length();//3
+        int i = 0;
+        int j = 0;
+        char[] res = new char[m+n];
+       while (i<res.length) {
+           if (j < m) {
+               res[i++] = word1.charAt(j);
+           }
+               if (j < n) {
+                   res[i++] = word2.charAt(j);
+               }
+               j++;
+           }
+        return new String(res);
+    }
+}
